30878d3708d569240ec23b77eb71f29b2e0a7373
6119greend <35507334+6119greend@users.noreply.github.com>
2018-05-01 12:55:55 -0500

    New

---
+++ app/src/main/java/com/silabs/eggboard/MyFirebaseInstanceIDService.java
@@ -0,0 +1,32 @@
+package com.silabs.eggboard;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Dominique on 4/3/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+    }
+
+
+
+}
